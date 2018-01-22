78e4566f793d0f1a53856f728291c77c19d922cf
Peter Street <PeterBStreet@gmail.com>
2018-01-21 21:52:12 -0700

    JS addEventListener for red, green, and blue

--- script.js
+++ script.js
@@ -9,9 +9,16 @@ function initColorPicker(){
 		green: document.getElementById("green"),
 		blue: document.getElementById("blue")
 	};
+	setColorPickerEventListener(colorBox, rgb);
 }
 
 function setColorPickerEventListener(colorBox, rgb) {
 	rgb.red.addEvenListener('change', () => {})
 	console.log("Red value: ", rgb.red.value);
-}
+})
+	rgb.green.addEventListener('change', ()) => {}
+	console.log("Green value: ", rgb.green.value);
+})
+rgb.blue.addEventListener('change', ()) => {}
+console.log("Blue value: ", rgb.blue.value);
+})
