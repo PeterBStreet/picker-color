7389fcc6c42db9ec2bb51e30a8c2f3fd9e9bbede
Peter Street <PeterBStreet@gmail.com>
2018-01-21 22:47:30 -0700

    JS add color slider function

--- script.js
+++ script.js
@@ -2,23 +2,32 @@ window.onload = function () {
 	initColorPicker();
 };
 
-function initColorPicker(){
+function initColorPicker() {
 	let colorBox = document.getElementById("color-box");
 	let rgb = {
 		red: document.getElementById("red"),
 		green: document.getElementById("green"),
 		blue: document.getElementById("blue")
 	};
-	setColorPickerEventListener(colorBox, rgb);
+	setColorPickerEventListeners(colorBox, rgb);
 }
 
-function setColorPickerEventListener(colorBox, rgb) {
-	rgb.red.addEvenListener('change', () => {})
+function setColorPickerEventListeners(colorBox, rgb) {
+	rgb.red.addEventListener('change', () => {
 	console.log("Red value: ", rgb.red.value);
-})
-	rgb.green.addEventListener('change', ()) => {}
+	setBoxBGColor(colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
+});
+	rgb.green.addEventListener('change', () => {
 	console.log("Green value: ", rgb.green.value);
-})
-rgb.blue.addEventListener('change', ()) => {}
-console.log("Blue value: ", rgb.blue.value);
-})
+	setBoxBGColor(colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
+});
+	rgb.blue.addEventListener('change', () => {
+	console.log("Blue value: ", rgb.blue.value);
+	setBoxBGColor(colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
+});
+}
+
+function setBoxBGColor(colorBox, red, green, blue) {
+	rgbVal = [red, green, blue].join(',');
+	colorBox.style.backgroundColor = "rgb(" + rgbVal + ")";
+}
